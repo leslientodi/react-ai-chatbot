758697810216bdbd8b4493fe414594581bf37770
leslientodi <lesliehazard500@gmail.com>
2024-12-17 09:25:06 +0000

    chatbot layout

--- AI-chatbot/src/App.jsx
+++ AI-chatbot/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import styles from "./App.module.css";
+import { Chat } from "./components/Chat";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState(MESSSAGES);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/chat-bot.png" />
+        <h2 className={styles.Title}>AI chatbot</h2>
+      </header>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+const MESSSAGES = [
+  {
+    role: "user",
+    content: "Lorem ipsum dolor",
+  },
+  {
+    role: "assistant",
+    content: "Lorem ipsum dolor",
+  },
+  {
+    role: "user",
+    content: "Lorem ipsum dolor",
+  },
+  {
+    role: "assistant",
+    content: "Lorem ipsum dolor",
+  },
+  {
+    role: "user",
+    content: "Lorem ipsum dolor",
+  },
+  {
+    role: "assistant",
+    content: "Lorem ipsum dolor",
+  },
+  {
+    role: "user",
+    content: "Lorem ipsum dolor",
+  },
+  {
+    role: "assistant",
+    content: "Lorem ipsum dolor",
+  },
+];
+
+export default App;
